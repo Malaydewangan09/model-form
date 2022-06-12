23e1f902986410a1cac6cef932dc1257dc73733f
Malay Dewangan <66718045+Malaydewangan09@users.noreply.github.com>
2022-06-13 00:25:59 +0530

    Update models.py

--- form_submission/app/models.py
+++ form_submission/app/models.py
@@ -7,7 +7,7 @@ class Customer(models.Model):
 	full_name = models.CharField(max_length=200)
         email = models.CharField(max_length=200)
         phone_number = models.CharField(max_length=200)
-	 date_of_birth = models.CharField(max_length=200)
+	date_of_birth = models.CharField(max_length=200)
 
 	def __str__(self):
 		return self.first_name + ' ' + self.last_name 
