a077e04fb4ef8bbf616d30e3a81abc9f149e22a0
Malay Dewangan <66718045+Malaydewangan09@users.noreply.github.com>
2022-06-13 00:17:42 +0530

    Update models.py

--- form_submission/app/models.py
+++ form_submission/app/models.py
@@ -4,8 +4,10 @@ from django.db import models
 
 
 class Customer(models.Model):
-	first_name = models.CharField(max_length=200)
-	last_name = models.CharField(max_length=200)
+	full_name = models.CharField(max_length=200)
+        email = models.CharField(max_length=200)
+        phone_number = models.CharField(max_length=200)
+	 date_of_birth = models.CharField(max_length=200)
 
 	def __str__(self):
 		return self.first_name + ' ' + self.last_name 
